eaa59578203356d371c7b5f37e4b34823d8ca393
Luke Diebold <luke@ldiebold.com>
2023-01-24 21:21:53 +1000

    remove tenant and org

--- app/config/orion-pinia-orm/orion-pinia-orm.ts
+++ app/config/orion-pinia-orm/orion-pinia-orm.ts
@@ -5,15 +5,11 @@ const orionPiniaOrm: OrionPluginOptions = {
     default: {
       apiEndpoint: process.env.CORE_API_URL + '/api',
       getRequestHeaders () {
-        const token = localStorage.getItem('fuellox-token')
-        const tenantId = localStorage.getItem('tenantId')
-        const orgId = localStorage.getItem('orgId')
+        const token = localStorage.getItem('token')
         return {
           Authorization: `Bearer ${token}`,
           'Content-Type': 'application/json',
           Accept: 'application/json',
-          'tenant-id': tenantId,
-          'org-id': orgId,
         }
       },
     },
