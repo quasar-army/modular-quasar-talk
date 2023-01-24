ada6a5b103a48a9f2afae1033ef028d7a946a8cd
Luke Diebold <luke@ldiebold.com>
2023-01-24 21:24:15 +1000

    home page

--- app/src/pages/IndexPage/IndexPage.vue
+++ app/src/pages/IndexPage/IndexPage.vue
@@ -1,23 +1,12 @@
 <script setup lang="ts">
-import { useFetchResources } from '@vuemodel/orion-pinia-orm'
-import Todo from 'modules/Todo/Todo'
 
-const todosFetcher = useFetchResources(Todo)
 </script>
 
 <template>
   <q-page
     padding
-    class="column flex-center"
+    class="column flex-center text-grey-8"
   >
-    <q-btn
-      label="fetch"
-      :loading="todosFetcher.fetching.value"
-      color="primary"
-      outline
-      @click="todosFetcher.fetch()"
-    />
-
-    <pre>{{ todosFetcher.repo.all() }}</pre>
+    You Can Build Anything...
   </q-page>
 </template>
